23324a5ac6924fcdbf9b30aad3aa9ebe7fd6f583
Wessel <34840@ma-web.nl>
2022-10-11 14:33:48 +0200

    code finished

--- Hello_You_Story.py
+++ Hello_You_Story.py
@@ -1,4 +1,3 @@
-from re import L
 import time
 
 
@@ -57,7 +56,59 @@ def civgo():
     ans = input("> ")
     if ans.lower().startswith("a"):
         fightcapture()
-    elif ans.lower().startswith("a"):
+    elif ans.lower().startswith("b"):
+        runcapture()
+    else:
+        capture()
 
+def fightcapture():
+    print("You attempt to fight them off, but eventually you are overpowered and taken away.")
+    end_locked_up()
+
+def runcapture():
+        print("You run until you cannot see the group following you.")
+        print("A. Keep Running     B. Hide In Bushes And Wait")
+        ans = input("> ")
+        if ans.lower().startswith("a"):
+            end_lost()
+        else:
+            hide()
+
+
+def hide():
+    print("You see them running past, but with 1 member less than you first saw. Without having time to process this, you hear something behind you, turn around and get knocked out by the (previously) missing member. ")
+    end_locked_up()
+
+def capture():
+    print("while they take you away you pass out")
+    end_locked_up()
+
+def end_die():
+    print("you fucking die")
+    print("The End.")
+
+def end_home():
+    print("You go home after a long day, waiting for the hallucinations to end. ")
+    print("The End.")
+
+def end_junk_leave():
+    print("You leave to return home, before you can enter your house, one of your neighbors asks you where you have been for the past 15 days. ")
+    print("The End.")
+
+def end_junk_stay():
+    print("You decide to sleep there. You eventually wake up in a dumpster with a crudely stitched up 	wound on the left side of your chest, below your ribcage. They stole your fucking kidney ")
+    print("The End.")
+
+def end_locked_up():
+    print("you wake up a few hours later in a jail cell, an officer tells you that you fought with another officer when they were investigating a disturbance about someone punching a kid.")
+    print("He also 	informs you that you're being charged for assault and resisting arrest")
+    print("The End.")
+
+def end_lost():
+    print("After walking for what seems like hours in the dark you collapse from exhaustion")
+    print("Upon waking up you find yourself in a ditch next to a field, not knowing what had happened the day prior. You decide never to do drugs again ")
+    print("The End.")
+
+# if this seems like a very half-arsed story, it is. I'm about as good of a creative writer as a slightly overdate bowl of chicken noodle soup
 
 intro()
